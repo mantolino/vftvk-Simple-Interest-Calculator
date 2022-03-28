24666601b80c960affa0897e7519ff5d9179a620
Marcello Antolino <marcello@antolino.net>
2022-03-28 22:59:11 +0000

    Exercise complete

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal == NaN || principal == "0" || parseFloat(principal) < 0)
+    {
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+        return;
+    }
+    principal = parseFloat(principal);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //create variable for shorter reference
+    var resultSpan = document.getElementById("result");
+
+    //use toString to avoid errors and improve readability on types
+    var newContent = "If you deposit \<span class='value'\>"+principal.toString()+"\</span\>,\<br\>";
+    newContent += "at an interest rate of \<span class='value'\>"+rate.toString()+"%\</span\>.\<br\>";
+    newContent += "You will receive an amount of \<span class='value'\>"+interest.toString()+"\</span\>,\<br\>";
+    newContent += "in the year \<span class='value'\>"+year.toString()+"\</span\>";
+
+    //first compute content; then assign content to HTML property
+    resultSpan.innerHTML = newContent;
+
 }
         
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
